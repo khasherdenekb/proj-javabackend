89368153471ce32eb5f78978b272fb9c21ca73ef
Khash-Erdene <khasherdeneswe@gmail.com>
2023-12-16 20:37:16 +0800

    added cors

---
+++ src/main/java/com/blogweb/backendjava/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.blogweb.backendjava.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        System.out.println("CorsConfig bean initialized!"); // Add this line for logging
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
